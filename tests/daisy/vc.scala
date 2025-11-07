ad4b4a5d6829a62f6431f0cad719f18135c79e60
lidaisy <li.daisy2003@gmail.com>
2025-11-07 09:52:22 -0500

    idek

--- tests/daisy/vc.scala
+++ tests/daisy/vc.scala
@@ -3,8 +3,6 @@ final class DFVal[+T, +M](val value: Int) extends AnyVal
 type DFValOf[+T] = DFVal[T, Any]
 
 class Top:
-//  val dmn1 = new scala.reflect.Selectable:
-//    val o: DFValOf[DFBit] = ???
-//  val x = dmn1.o
-
-  val o: DFValOf[DFBit] = ???
+  val dmn1 = new scala.reflect.Selectable:
+    val o: DFValOf[DFBit] = ???
+  val x = dmn1.o
